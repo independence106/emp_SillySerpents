b1261d7476e2db3b667a92b17e2a7be630a86a99
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-12-16 20:07:37 -0500

    Create LinSearch.java

---
+++ LinSearch.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class Main{
+ public static long guessTimer(int[] list, int target){
+     // Determines time required (in ms) to locate target (using linear search)
+     
+     long initialTime = System.currentTimeMillis();
+     for(int i = 0; i < list.length; i++){
+         if(list[i] == target) break;
+     }
+     
+    long finalTime = System.currentTimeMillis();
+    
+    return (finalTime - initialTime);   
+ }
+ 
+ 
+ 
+ public static String maxTimeGuesses(int numElements){
+    // Determine the maximum amount of time required to find any certain element in a sorted list of a given amount of elements (w/ binary search)    
+     
+     // Create a list with numElements as many elements, and populate it with index = elements to ensure ascending order 
+     int[] list = new int[numElements];
+     for (int i = 0; i < numElements; i++)  list[i] = i;
+     
+    long guessTime;
+    long maxTime = 0;
+     
+     for(int i = 0; i < numElements; i++){
+         guessTime = guessTimer(list, i);
+         if (guessTime > maxTime)    maxTime = guessTime; 
+     }
+     
+     //  Return the maximum time to determine an element
+     return numElements + ") " + maxTime + " ms.";
+ }
+ 
+ public static void main(String[] args) {
+      
+      System.out.println(maxTimeGuesses(200000));
+      
+      
+  
+ }
+}
